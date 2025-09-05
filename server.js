5db0c8236747b96d7bdcf864d07c80e395fdfd89
=leo-ema100 <=lgarciag45@miumg.edu.gt>
2025-09-05 23:11:47 +0200

    set up a Template Engine

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+  // Change the response to render the Pug template
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log(`Listening on port ${PORT}`);
 });
